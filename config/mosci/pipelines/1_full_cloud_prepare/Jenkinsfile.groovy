86d2b11b9930af107b68c528ebf8c9ec5f5413ff
Andrew McLeod <andrew.mcleod@canonical.com>
2019-04-08 00:19:02 +0200

    asdasd

--- config/mosci/pipelines/1_full_cloud_prepare/Jenkinsfile.groovy
+++ config/mosci/pipelines/1_full_cloud_prepare/Jenkinsfile.groovy
@@ -216,7 +216,7 @@ node("${SLAVE_NODE_NAME}") {
                         readContent = readFile 'juju-configs/clouds.yaml'
                         LXD_UNDER = LXD_IP.replaceAll("[.]", "-")
                         if ( ! readContent.contains("${LXD_UNDER}")) {
-                            writeFile file: 'juju-configs/clouds.yaml', text: readContent + "\n  lxd-${LXD_UNDER}:\n    type: lxd\n    auth-types: [interactive, certificate]\n    endpoint: https://${LXD_UNDER}:8443\n"
+                            writeFile file: 'juju-configs/clouds.yaml', text: readContent + "\n  lxd-${LXD_UNDER}:\n    type: lxd\n    auth-types: [interactive, certificate]\n    endpoint: https://${LXD_IP}:8443\n"
                         }
                         readContent = readFile 'juju-configs/credentials.yaml'
                         if ( ! readContent.contains("${LXD_UNDER}"))  {
@@ -229,7 +229,7 @@ node("${SLAVE_NODE_NAME}") {
                         try {
                             readContent = readFile 'clouds.yaml'
                             if ( ! readContent.contains("${LXD_UNDER}")) {
-                                writeFile file: 'clouds.yaml', text: readContent + "\n  lxd-${LXD_UNDER}:\n    type: lxd\n    auth-types: [interactive, certificate]\n    endpoint: https://${LXD_UNDER}:8443\n"
+                                writeFile file: 'clouds.yaml', text: readContent + "\n  lxd-${LXD_UNDER}:\n    type: lxd\n    auth-types: [interactive, certificate]\n    endpoint: https://${LXD_IP}:8443\n"
                             }
                         } catch(all) {
                             sh "cp ${env.HOME}/tools/charm-test-infra/juju-configs/clouds.yaml ."
