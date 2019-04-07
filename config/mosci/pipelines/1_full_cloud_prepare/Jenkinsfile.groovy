302aa87c18dec0bf4c6c7b89c55520df8cae06bf
Andrew McLeod <andrew.mcleod@canonical.com>
2019-04-07 22:23:37 +0200

    ush

--- config/mosci/pipelines/1_full_cloud_prepare/Jenkinsfile.groovy
+++ config/mosci/pipelines/1_full_cloud_prepare/Jenkinsfile.groovy
@@ -208,8 +208,9 @@ node("${SLAVE_NODE_NAME}") {
                     echo "${CHECK_AUTH}"
                     dir("${env.HOME}/tools/charm-test-infra") {
                         def readContent = readFile 'juju-configs/clouds.yaml'
-                        if ( ! readContent.contains("${LXD_IP}")) {
-                                writeFile file: 'juju-configs/clouds.yaml', text: readContent + "\nlxd-${LXD_IP}\n  type:lxd\n  auth-types: [interactive,certificate]\n  endpoint: https://${LXD_IP}:8443\n"
+                        LXD_UNDER = "${LXD_IP}.replaceAll("[.]", "-")"
+                        if ( ! readContent.contains("${LXD_UNDER}")) {
+                                writeFile file: 'juju-configs/clouds.yaml', text: readContent + "\nlxd-${LXD_UNDER}\n  type:lxd\n  auth-types: [interactive,certificate]\n  endpoint: https://${LXD_UNDER}:8443\n"
                         }
                         /*sh "sed -i 's/__ENDPOINT_LXD__/${params.LXD_IP}/g' juju-configs/clouds.yaml"
                         sh "cat juju-configs/clouds.yaml"*/
