f3d72bf29aff6066063debfc78faec5253432bb6
Andrew McLeod <andrew.mcleod@canonical.com>
2019-04-08 00:02:46 +0200

    pewm

--- config/mosci/pipelines/2_full_cloud_bootstrap/Jenkinsfile.groovy
+++ config/mosci/pipelines/2_full_cloud_bootstrap/Jenkinsfile.groovy
@@ -371,6 +371,7 @@ node(params.SLAVE_NODE_NAME) {
                         env.MODEL_NAME=MODEL_NAME
                         echo "MODEL_NAME: ${env.MODEL_NAME}, CONTROLLER_NAME: ${env.CONTROLLER_NAME}"
                         env.OS_PROJECT_NAME="${params.ARCH}-mosci"
+                        env.CLOUD_NAME=CLOUD_NAME
                         sh "./juju-maas-controller-mosci.sh"
                     }
                 }
