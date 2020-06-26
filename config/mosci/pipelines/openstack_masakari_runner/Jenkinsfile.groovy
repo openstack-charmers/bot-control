50fe480bcf2ce67159e2c20f0cc5ae85f1b935f3
Andrew McLeod <andrew.mcleod@canonical.com>
2020-06-26 12:35:15 +0200

    add masakari test

--- config/mosci/pipelines/openstack_masakari_runner/Jenkinsfile.groovy
+++ config/mosci/pipelines/openstack_masakari_runner/Jenkinsfile.groovy
@@ -1,15 +1,17 @@
-TESTS = ${params.SELECTED_TESTS} + ",zaza test - openstack - zaza masakari"
+TESTS = "${params.SELECTED_TESTS},zaza test - openstack - zaza masakari"
+FALSE = False
+TRUE = True
 
 node('master') {
     stage('Trigger 0. Full Cloud - Pipeline') {
         trigger_wrapper = build job: '0. Full Cloud - Pipeline', parameters: 
-        [[$class: 'BooleanParameterValue', name: 'OPENSTACK', value: "True"],
+        [[$class: 'BooleanParameterValue', name: 'OPENSTACK', value: Boolean.valueOf(TRUE)],
         [$class: 'StringParameterValue', name: 'CLOUD_NAME', value: params.CLOUD_NAME],
         [$class: 'StringParameterValue', name: 'ARCH', value: params.ARCH],
         [$class: 'StringParameterValue', name: 'DISPLAY_NAME', value: params.DISPLAY_NAME],
         [$class: 'StringParameterValue', name: 'SLAVE_NODE_NAME', value: params.SLAVE_NODE_NAME],
-        [$class: 'BooleanParameterValue', name: 'CLEANUP_ON_FAILURE', value: "False"],
-        [$class: 'BooleanParameterValue', name: 'FORCE_NEW_CONTROLLER', value: "False"],
+        [$class: 'BooleanParameterValue', name: 'CLEANUP_ON_FAILURE', value: Boolean.valueOf(FALSE)],
+        [$class: 'BooleanParameterValue', name: 'FORCE_NEW_CONTROLLER', value: Boolean.valueOf(FALSE)],
         [$class: 'StringParameterValue', name: 'BUNDLE_URL', value: params.BUNDLE_URL],
         [$class: 'StringParameterValue', name: 'BUNDLE_OVERLAYS', value: params.BUNDLE_OVERLAYS],
         [$class: 'StringParameterValue', name: 'OVERRIDE_BUNDLE_CONFIG', value: params.OVERRIDE_BUNDLE_CONFIG],
